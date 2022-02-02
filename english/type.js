0bda4da30534b70df85ecb3a1de75aa82316f99a
JL Design <97342648+jldesing-dev@users.noreply.github.com>
2022-02-02 13:40:47 -0400

    Add files via upload

---
+++ english/type.js
@@ -0,0 +1,7 @@
+var typing = new Typed(".typing", {
+
+    strings: ["Designer", "Web Developer", "Backend Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
---
+++ type.js
@@ -0,0 +1,7 @@
+var typing = new Typed(".typing", {
+
+    strings: ["Diseñador", "Programador Web", "Programador Backend", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
